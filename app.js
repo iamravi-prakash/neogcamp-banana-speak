e85c5e63eafa383fb7b789c015bc2e75fd5472ba
Ravi Prakash <57127635+iamravi-prakash@users.noreply.github.com>
2022-12-21 19:05:04 +0530

    js imported

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("Hello! hru");
+
+prompt("r u 18+?");
+
+var username = prompt("give me username");
+
+var welcomeMesseage = "this script is working " + username;
+
+alert(welcomeMesseage);
