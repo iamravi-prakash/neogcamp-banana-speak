e0a634e9dc3428d20d060be01e63682669bc2d92
Ravi Prakash <57127635+iamravi-prakash@users.noreply.github.com>
2022-12-22 09:44:00 +0530

    input output process done

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ var btnTranslate = document.querySelector("#click");
 function clickEvent() {
   console.log("clicked!");
   console.log("input", textInput.value);
+  document.querySelector("#txt-output").innerText =
+    "ajajajajaj + " + textInput.value;
 }
 
 btnTranslate.addEventListener("click", clickEvent);
 
 var textInput = document.querySelector("#txt-input");
+
+// var textOutput = document.querySelector("txt-output") --> mistake "#"
+// document.querySelector("#txt-output").innerText = "ravi";
